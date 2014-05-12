5acdd4a0c8a4473e89ce47bbbc58829765e474e1
Patrick Lavallee <lavalleep@gmail.com>
2014-05-12 02:31:39 -0400

    Modification au script de déploiement, ajout des concentrations au dataload

--- src/database/deploy-database.sql
+++ src/database/deploy-database.sql
@@ -2,7 +2,7 @@ CREATE TABLE Evenements (id  SERIAL NOT NULL, Clubs_id int4 NOT NULL, nom varcha
 CREATE TABLE Contacts_Urgence_Membres (Contacts_Urgence_id int4 NOT NULL UNIQUE, Membres_id int4 NOT NULL UNIQUE, PRIMARY KEY (Contacts_Urgence_id, Membres_id));
 CREATE TABLE Membres_Clubs_Roles (Roles_id int4 NOT NULL UNIQUE, Membres_Clubs_id int4 NOT NULL UNIQUE, PRIMARY KEY (Roles_id, Membres_Clubs_id));
 CREATE TABLE Audits (id  SERIAL NOT NULL, username varchar(50) NOT NULL, message varchar(255), timestamp timestamp NOT NULL, PRIMARY KEY (id));
-CREATE TABLE Membres_Preferences (id  SERIAL NOT NULL, "key" varchar(500) NOT NULL, value varchar(150) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE Membres_Preferences (id  SERIAL NOT NULL, membres_id int4 NOT NULL, "key" varchar(50) NOT NULL, value varchar(150) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Roles (id  SERIAL NOT NULL, nom varchar(50) NOT NULL, description varchar(255) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Allergies (id  SERIAL NOT NULL, description varchar(128) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Membres_Allergies (Membres_id int4 NOT NULL UNIQUE, Allergies_id int4 NOT NULL UNIQUE, PRIMARY KEY (Membres_id, Allergies_id));
@@ -18,10 +18,11 @@ CREATE TABLE Fournisseurs_Items (Fournisseurs_id int4 NOT NULL UNIQUE, Items_id 
 CREATE TABLE Fournisseurs (id  SERIAL NOT NULL, Adresses_id int4 NOT NULL, nom varchar(255) NOT NULL, contact varchar(64) NOT NULL, telephone varchar(24) NOT NULL, fax varchar(24), courriel varchar(255), PRIMARY KEY (id));
 CREATE TABLE Suivie_Statuts (id  SERIAL NOT NULL, code varchar(50) NOT NULL, description varchar(150) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Concentrations (id  SERIAL NOT NULL, acronyme varchar(10) NOT NULL, description varchar(150), PRIMARY KEY (id));
-CREATE TABLE Membres (id  SERIAL NOT NULL, Concentrations_id int4 NOT NULL, Membres_Preferences_id int4, nom varchar(64) NOT NULL, prenom varchar(64) NOT NULL, courriel varchar(255) NOT NULL, code_permanent varchar(15) NOT NULL, actif bool NOT NULL, telephone varchar(16), PRIMARY KEY (id));
+CREATE TABLE Membres (id  SERIAL NOT NULL, Concentrations_id int4 NOT NULL, nom varchar(64) NOT NULL, prenom varchar(64) NOT NULL, courriel varchar(255) NOT NULL, code_permanent varchar(15) NOT NULL, code_universel varchar(10) NOT NULL, actif bool NOT NULL, telephone varchar(16), PRIMARY KEY (id));
 CREATE TABLE Membres_Clubs (id  SERIAL NOT NULL, Clubs_id int4 NOT NULL, Membres_id int4 NOT NULL, date_debut date NOT NULL, date_fin date, PRIMARY KEY (id));
 CREATE TABLE Adresses (id  SERIAL NOT NULL, no_civique int4 NOT NULL, rue varchar(64) NOT NULL, appartement varchar(10), ville varchar(128) NOT NULL, code_postal varchar(16) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Clubs (id  SERIAL NOT NULL, nom varchar(50) NOT NULL, description varchar(255), PRIMARY KEY (id));
+ALTER TABLE Contacts_Urgence ADD CONSTRAINT FKContacts_U164890 FOREIGN KEY (liens_parente_id) REFERENCES Liens_Parente (id);
 ALTER TABLE Evenements ADD CONSTRAINT FKEvenements917475 FOREIGN KEY (Clubs_id) REFERENCES Clubs (id);
 ALTER TABLE Membres ADD CONSTRAINT FKMembres495106 FOREIGN KEY (Concentrations_id) REFERENCES Concentrations (id);
 ALTER TABLE Suivies ADD CONSTRAINT FKSuivies436259 FOREIGN KEY (Membres_id) REFERENCES Membres (id);
@@ -32,7 +33,6 @@ ALTER TABLE Membres_Formations ADD CONSTRAINT FKMembres_Fo906962 FOREIGN KEY (Fo
 ALTER TABLE Fournisseurs_Items ADD CONSTRAINT FKFournisseu723122 FOREIGN KEY (Fournisseurs_id) REFERENCES Fournisseurs (id);
 ALTER TABLE Fournisseurs_Items ADD CONSTRAINT FKFournisseu24556 FOREIGN KEY (Items_id) REFERENCES Items (id);
 ALTER TABLE Suivies ADD CONSTRAINT FKSuivies572159 FOREIGN KEY (Commandites_id) REFERENCES Commandites (id);
-ALTER TABLE Membres ADD CONSTRAINT FKMembres46393 FOREIGN KEY (Membres_Preferences_id) REFERENCES Membres_PREFERENCES (id);
 ALTER TABLE Items ADD CONSTRAINT FKItems252341 FOREIGN KEY (Units_id) REFERENCES Units (id);
 ALTER TABLE Fournisseurs ADD CONSTRAINT FKFournisseu604575 FOREIGN KEY (Adresses_id) REFERENCES Adresses (id);
 ALTER TABLE Commandites ADD CONSTRAINT FKCommandite968053 FOREIGN KEY (Fournisseurs_id) REFERENCES Fournisseurs (id);
@@ -45,4 +45,4 @@ ALTER TABLE Membres_Clubs ADD CONSTRAINT FKMembres_Cl269114 FOREIGN KEY (Clubs_i
 ALTER TABLE Membres_Clubs ADD CONSTRAINT FKMembres_Cl936196 FOREIGN KEY (Membres_id) REFERENCES Membres (id);
 ALTER TABLE Contacts_Urgence_Membres ADD CONSTRAINT FKContacts_U960303 FOREIGN KEY (Contacts_Urgence_id) REFERENCES Contacts_Urgence (id);
 ALTER TABLE Contacts_Urgence_Membres ADD CONSTRAINT FKContacts_U30385 FOREIGN KEY (Membres_id) REFERENCES Membres (id);
-ALTER TABLE Contacts_Urgence ADD CONSTRAINT FKContacts_U164890 FOREIGN KEY (liens_parente_id) REFERENCES Liens_Parente (id);
+ALTER TABLE Membres_Preferences ADD CONSTRAINT FKMembres_Pr229875 FOREIGN KEY (membres_id) REFERENCES Membres (id);
--- src/database/initial-dataload.sql
+++ src/database/initial-dataload.sql
@@ -1,3 +1,12 @@
 ﻿Insert into clubs values (nextval('clubs_id_seq'), 'PRECI','Programme de Regroupement Étudiant pour la Coopération Internationale');
 Insert into clubs values (nextval('clubs_id_seq'), 'LiETS','Ligue Improvisation ETS');
 Insert into clubs values (nextval('clubs_id_seq'), 'Evolution','Vehicule à faible consommation');
+
+Insert into concentrations values (nextval('concentrations_id_seq'), 'CTN','Génie de la construction');
+Insert into concentrations values (nextval('concentrations_id_seq'), 'ELE','Génie électrique');
+Insert into concentrations values (nextval('concentrations_id_seq'), 'GOL','Génie des opérations et de la logistique');
+Insert into concentrations values (nextval('concentrations_id_seq'), 'GPA','Génie de production automatisée');
+Insert into concentrations values (nextval('concentrations_id_seq'), 'LOG','Génie logiciel');
+Insert into concentrations values (nextval('concentrations_id_seq'), 'MEC','Génie mécanique');
+Insert into concentrations values (nextval('concentrations_id_seq'), 'TI','Génie des technologies de l`information');
+
