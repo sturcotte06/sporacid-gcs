ab217767e6b9ae5220534a74f48bf03fc34730a3
jeanberniervibert@gmail.com <jeanberniervibert@gmail.com>
2014-06-01 19:10:44 -0400

    creation initial

--- src/gcs.webservices/src/gcs/webservices/services/CommanditeService.java
+++ src/gcs.webservices/src/gcs/webservices/services/CommanditeService.java
@@ -1,15 +1,28 @@
 package gcs.webservices.services;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import gcs.webapp.utils.MessageType;
 import gcs.webapp.utils.app.security.CrudOperation;
 import gcs.webapp.utils.app.security.CrudOperator;
 import gcs.webapp.utils.app.security.SecureModule;
 import gcs.webservices.client.beans.ContextualSessionToken;
+import gcs.webservices.client.requests.commandites.AddRequest;
+import gcs.webservices.client.requests.commandites.DeleteRequest;
 import gcs.webservices.client.responses.DummyResponse;
 import gcs.webservices.dao.ICommanditeDao;
+import gcs.webservices.dao.IFournisseurDao;
+import gcs.webservices.dao.IItemDao;
+import gcs.webservices.models.Club;
+import gcs.webservices.models.Commandite;
+import gcs.webservices.models.Membre;
+import gcs.webservices.models.Suivie;
 
 import javax.ws.rs.BeanParam;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
@@ -23,7 +36,13 @@ public class CommanditeService extends SecureHttpService
 {
     @Autowired
     private ICommanditeDao commanditeDao;
-
+    
+    @Autowired
+    private IFournisseurDao fournisseurDao;
+    
+    @Autowired
+    private IItemDao itemDao;
+    
     @GET
     @CrudOperator(CrudOperation.Read)
     public Response getAll(@BeanParam ContextualSessionToken sessionToken)
@@ -36,7 +55,61 @@ public class CommanditeService extends SecureHttpService
 
         return completeRequest(dummyResponse);
     }
-
+    
+    @POST
+    @CrudOperator(CrudOperation.Create)
+    public Response add(@BeanParam ContextualSessionToken sessionToken, AddRequest request)
+    {
+        DummyResponse dummyResponse = new DummyResponse();
+        Commandite commandite = new Commandite();
+        
+        //dummyResponse.setObject();
+        dummyResponse.setSuccess(true);
+        dummyResponse.addMessage(MessageType.Information, "Success");
+        
+        commandite.setFournisseur(fournisseurDao.getFournisseurById(request.getIdFournisseur()));
+        commandite.setItem(itemDao.getItemById(request.getIdItem()));
+        commandite.setNature(request.getNature());
+        commandite.setValeur(request.getValeur());
+        
+        /* Création du suivie de départ */
+        Suivie suivie = new Suivie();
+        suivie.setCommentaire("Création initiale");
+        suivie.setDateSuivie(new Date());
+        //suivie.setSuivieStatut();
+        
+        super.sessionCache.withSession(sessionToken, session->{
+        	Membre membre = session.getMembre();
+        	Club club = session.getMembre().getClubByName(sessionToken.getContext().getName());
+        	
+        	commandite.setClub(club);
+        	suivie.setMembreId(membre.getId());
+        });
+        
+        return completeRequest(dummyResponse);
+    }
+    
+    @DELETE
+    @CrudOperator(CrudOperation.Delete)
+    public Response delete(@BeanParam ContextualSessionToken sessionToken, DeleteRequest request)
+    {
+        DummyResponse dummyResponse = new DummyResponse();
+        ArrayList<Commandite> commandites = (ArrayList<Commandite>) commanditeDao.getAllCommandite();
+        
+        for (Commandite commandite : commandites) {
+			if (commandite.getId() == request.getId()) {
+				//TODO
+				break;
+			}
+		}
+        //TODO
+        
+        //dummyResponse.setObject();
+        dummyResponse.setSuccess(true);
+        dummyResponse.addMessage(MessageType.Information, "Success");
+        
+        return completeRequest(dummyResponse);
+    }
     /**
      * @return the commanditeDao
      */
--- src/gcs.webservices/src/gcs/webservices/services/FournisseurService.java
+++ src/gcs.webservices/src/gcs/webservices/services/FournisseurService.java
@@ -1,9 +1,18 @@
 package gcs.webservices.services;
 
+import java.util.Collection;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import gcs.webapp.utils.app.security.SecureModule;
+import gcs.webapp.utils.exceptions.InternalException;
+import gcs.webservices.dao.IClubDao;
+import gcs.webservices.dao.IFournisseurDao;
+import gcs.webservices.models.Club;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,5 +20,14 @@ import org.springframework.stereotype.Component;
 @Path("/context/{contextName}/session/{ipv4Address}/{sessionKey}/fournisseur")
 public class FournisseurService extends SecureHttpService
 {
-    
+	@Autowired
+    private IFournisseurDao fournisseurDao;
+
+    @GET
+    // @Path("/")
+    public Response getClubs()
+    {
+        //TOOD
+    	return null;
+    }
 }
