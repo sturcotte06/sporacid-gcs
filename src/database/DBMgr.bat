ad64e7c9e7a69512a26c5adc9d08dc7b75f24131
decapent <lavalleep@gmail.com>
2014-06-08 16:48:49 -0400

    Mise a jour du Manager de BD pour rouler les bon scripts

--- src/database/DBMgr.bat
+++ src/database/DBMgr.bat
@@ -8,19 +8,17 @@ REM ----------------------------------------------------
 @ECHO OFF
 SET host="127.0.0.1"
 SET username="postgres"
-SET password="qazwsxedc"
+SET password="bacon"
 SET port=5432
-SET pgShell="E:\Program Files (x86)\PostGre\bin\psql.exe"
+SET pgShell="C:\Program Files\PostgreSQL\9.3\bin\psql.exe"
 
 :DisplayMenu
 REM  // Clear screen, print menu and read user input
 CLS
 ECHO -- Database Manager (Sporacid studios 2013) --
 ECHO.
-ECHO  1- Build Tables
+ECHO  1- Build Tables And dataload
 ECHO  2- Drop Tables
-ECHO  3- Cleanup Tables
-ECHO  4- Dataload Database
 ECHO  0- Quit
 ECHO.
 ECHO.
@@ -36,8 +34,6 @@ ECHO.
 REM // decisions... decisions...
 IF %decision%==1 GOTO :Build 
 IF %decision%==2 GOTO :Destroy
-IF %decision%==3 GOTO :Cleanup
-IF %decision%==4 GOTO :Dataload
 IF %decision%==0 (
 	GOTO :Finally
 ) ELSE (
@@ -46,7 +42,8 @@ IF %decision%==0 (
 
 :Build
 ECHO Building table structure of: %dbname%
-%pgShell% -h %host% -U %username% -d %dbname% -p %port% -f ./DDL_create_all.sql
+%pgShell% -h %host% -U %username% -d %dbname% -p %port% -f ./deploy-database.sql
+%pgShell% -h %host% -U %username% -d %dbname% -p %port% -f ./initial-dataload.sql
 ECHO.
 ECHO Database built!
 PAUSE
@@ -54,25 +51,9 @@ GOTO :DisplayMenu
 
 :Destroy
 ECHO Dropping table structure of: %dbname%
-%pgShell% -h %host% -U %username% -d %dbname% -p %port% -f ./DDL_destroy_all.sql
+%pgShell% -h %host% -U %username% -d %dbname% -p %port% -f ./drop-database.sql
 ECHO.
 ECHO Database destroyed!
-PAUSE
-GOTO :DisplayMenu
-
-:Cleanup
-ECHO Deleting %dbname%'s tables
-%pgShell% -h %host% -U %username% -d %dbname% -p %port% -f ./DML_delete_all.sql
-ECHO.
-ECHO Database tables cleaned!
-PAUSE
-GOTO :DisplayMenu
-
-:Dataload
-ECHO Dataloading %dbname%'s tables
-%pgShell% -h %host% -U %username% -d %dbname% -p %port% -f ./DML_insert_all.sql
-ECHO.
-ECHO Database tables dataloaded!
 PAUSE
 GOTO :DisplayMenu
 
