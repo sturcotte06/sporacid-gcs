afbb68b8a015f5148a25d3f1bb8cb26803e4d687
decapent <lavalleep@gmail.com>
2014-06-08 00:48:11 -0400

    sdfghjm

---
+++ src/gcs.webservices.client/src/gcs/webservices/client/responses/enums/GetConcentrationResponse.java
@@ -0,0 +1,10 @@
+package gcs.webservices.client.responses.enums;
+
+import gcs.webservices.client.models.ConcentrationBean;
+import gcs.webservices.client.responses.ResponseWithEntity;
+
+import java.util.Collection;
+
+public class GetConcentrationResponse extends ResponseWithEntity<Collection<ConcentrationBean>> {
+
+}
